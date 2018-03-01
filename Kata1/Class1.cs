4c6a8df182ab27093ec4d6a5d206d597a46dbe98
Timmus <tagersea@gmail.com>
2018-03-01 00:28:24 -0700

    Complete v1

--- Kata1/Class1.cs
+++ Kata1/Class1.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace Kata1
 {
     public class Class1
     {
+        public int chop(int needle, int[] haystack)
+        {
+            if (haystack == null || haystack.Length == 0)
+            {
+                return -1;
+            }            
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
     }
 }
--- Kata1Tests/UnitTest1.cs
+++ Kata1Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Kata1;
 using System;
 using Xunit;
 
@@ -5,10 +6,66 @@ namespace Kata1Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        Class1 sut;
 
+        public UnitTest1()
+        {
+            sut = new Class1();
+        }
+
+        [Fact]
+        public void NullArrayReturnsNegOne()
+        {
+            Assert.Equal(-1, sut.chop(3, null));
+        }
+
+        [Fact]
+        public void EmptyArrayReturnsNegOne()
+        {
+            Assert.Equal(-1, sut.chop(3, new int[] {}));
+        }
+
+        [Fact]
+        public void InvalidMatchInSingleItemArrayReturnsNegOne()
+        {
+            Assert.Equal(-1, sut.chop(3, new int[] { 1 }));
+        }
+
+        [Fact]
+        public void ValidMatchInSingleItemArrayReturnsZero()
+        {
+            Assert.Equal(0, sut.chop(1, new int[] { 1 }));
+        }
+
+        [Theory]
+        [InlineData(0, 1, new int[] { 1, 3, 5 })]
+        [InlineData(-1, 1, new int[] { 2, 3, 5 })]
+        [InlineData(1, 3, new int[] { 1, 3, 5 })]
+        [InlineData(2, 5, new int[] { 1, 3, 5 })]
+        [InlineData(-1, 0, new int[] { 1, 3, 5 })]
+        [InlineData(-1, 2, new int[] { 1, 3, 5 })]
+        [InlineData(-1, 4, new int[] { 1, 3, 5 })]
+        [InlineData(-1, 6, new int[] { 1, 3, 5 })]
+        public void valiateThreeItemHaystacks(
+            int expected, int needle, int[] haystack)
+        {
+            Assert.Equal(expected, sut.chop(needle, haystack));
+        }
+
+        [Theory]
+        [InlineData(0, 1, new int[] { 1, 3, 5, 7 })]
+        [InlineData(1, 3, new int[] { 1, 3, 5, 7 })]
+        [InlineData(2, 5, new int[] { 1, 3, 5, 7 })]
+        [InlineData(3, 7, new int[] { 1, 3, 5, 7 })]
+        [InlineData(-1, 0, new int[] { 1, 3, 5, 7 })]
+        [InlineData(-1, 2, new int[] { 1, 3, 5, 7 })]
+        [InlineData(-1, 4, new int[] { 1, 3, 5, 7 })]
+        [InlineData(-1, 6, new int[] { 1, 3, 5, 7 })]
+        [InlineData(-1, 8, new int[] { 1, 3, 5, 7 })]
+        public void valiateFourItemHaystacks(
+    int expected, int needle, int[] haystack)
+        {
+            Assert.Equal(expected, sut.chop(needle, haystack));
         }
     }
 }
